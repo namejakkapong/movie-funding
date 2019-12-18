d0a81307844d079d6c9617462cefbde6dc317871
namejakkapong <name_jakkapong@hotmail.com>
2019-12-18 14:09:22 +0700

    add template

--- admin/src/app/app-routing.module.ts
+++ admin/src/app/app-routing.module.ts
@@ -1,11 +1,62 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthLayoutComponent } from './shared/components/layouts/auth-layout/auth-layout.component';
+import { AuthGaurd } from './shared/services/auth.gaurd';
+import { BlankLayoutComponent } from './shared/components/layouts/blank-layout/blank-layout.component';
+import { AdminLayoutSidebarCompactComponent } from './shared/components/layouts/admin-layout-sidebar-compact/admin-layout-sidebar-compact.component';
 
 
-const routes: Routes = [];
+const adminRoutes: Routes = [
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./views/dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {
+    path: 'users',
+    loadChildren: () => import('./views/users/users.module').then(m => m.UsersModule)
+  },
+];
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard/default',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: 'sessions',
+        loadChildren: () => import('./views/sessions/sessions.module').then(m => m.SessionsModule)
+      }
+    ]
+  },
+  {
+    path: '',
+    component: BlankLayoutComponent,
+    children: [
+      {
+        path: 'others',
+        loadChildren: () => import('./views/others/others.module').then(m => m.OthersModule)
+      }
+    ]
+  },
+  {
+    path: '',
+    component: AdminLayoutSidebarCompactComponent,
+    canActivate: [AuthGaurd],
+    children: adminRoutes
+  },
+  {
+    path: '**',
+    redirectTo: 'others/404'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
